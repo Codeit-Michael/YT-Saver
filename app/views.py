75465d3a7c8094454c63bcbb73fdf3e17a4900af
Michael Maranan <michaelmaranan030@gmail.com>
2022-01-28 20:23:17 +0800

    fetching video worked

--- app/views.py
+++ app/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+import re
+from django.shortcuts import render, redirect
+from pytube import YouTube
 
 # Create your views here.
+def home(request):
+    myTitle = ''
+    myQuality = None
+    if request.method == 'POST':
+        link = request.POST['given_url']
+        myVideo = YouTube(link)
+        myTitle = myVideo.title
+        myQuality = myVideo.streams        
+    context = {'vid':myTitle,'qual':myQuality}
+    return render(request,'app/home.html',context)
+
+# PROBLEM: HOW TO SELECT AND DOWNLOAD THE RESOLUTION CHOSEN
