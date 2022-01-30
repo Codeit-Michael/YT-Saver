30b357ab37bcc08e43349fc1d7ee900bdee1d925
Michael Maranan <michaelmaranan030@gmail.com>
2022-01-30 17:51:08 +0800

    moved download path to 'Downloads'

--- app/views.py
+++ app/views.py
@@ -30,32 +30,6 @@ class home(View):
             self.url = request.POST.get('given_url')
             video = YouTube(self.url)
             stream = [x for x in video.streams.filter(progressive=True)]
-            video.streams[int(request.POST.get('download-vid')) - 1].download()
+            video.streams[int(request.POST.get('download-vid')) - 1].download(output_path='../../Downloads')
 
         return render(request,'app/home.html')
-        
-
-
-
-# def home(request,*args,**kwargs):
-#     if request.method == 'POST':
-#         url = request.POST['given_url']
-#         video = YouTube(url)
-#         vidTitle,vidThumbnail = video.title,video.thumbnail_url
-#         qual,stream = [],[]
-#         for vid in video.streams.filter(progressive=True):
-#             qual.append(vid.resolution)
-#             stream.append(vid)
-#         context = {'vidTitle':vidTitle,'vidThumbnail':vidThumbnail,
-#                     'qual':qual,'stream':stream,
-#                     'url':url}
-#         return render (request,'app/home.html',context)
-
-#     return render(request,'app/home.html')
-
-
-# def downloadVid(request,id,url):
-#     print(id,url)
-#     return redirect('home')
-
-# PROBLEM: HOW TO SELECT AND DOWNLOAD THE RESOLUTION CHOSEN
